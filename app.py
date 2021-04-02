778a0205b40bf7eae897a3ff1f2eff6408310f9c
Prashant Gaikwad <66205621+prashantgaikwad132@users.noreply.github.com>
2021-04-02 18:19:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, request
 
 import numpy as np
-import os
 
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
