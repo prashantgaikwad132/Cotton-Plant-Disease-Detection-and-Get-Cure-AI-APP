4be6777946e311093b943c76d3d735435ba134a5
Prashant Gaikwad <66205621+prashantgaikwad132@users.noreply.github.com>
2021-11-25 19:47:56 +0530

    Update model.py

--- model.py
+++ model.py
@@ -138,25 +138,3 @@ history = cnn_model.fit(training_data,
 
 
 history.history
-
-
-# In[15]:
-
-
-# summarize history for accuracy
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
